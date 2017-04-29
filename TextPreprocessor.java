59bae54c77f5eb610b4bce83814f2a421f4c3b1e
viseshrp <viseshrprasad@gmail.com>
2017-04-29 16:03:45 -0400

    TextPreprocessing : Read all files in a folder and output multiple files into another folder

--- TextPreprocessor.java
+++ TextPreprocessor.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
@@ -13,36 +14,38 @@ public class TextPreprocessor {
 
 	public static void main(String[] args) {
 		try {
-			Path pt = new Path(args[0]);
+
 			FileSystem fs = FileSystem.get(new Configuration());
-			BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
-			String lineRead;
-			lineRead = br.readLine();
-			String doc = "";
-			
-			boolean value = false;
-			while (lineRead != null) {
-				
-				if(value) {
-				doc += lineRead;
-				
-				}
-				value = true;
+			FileStatus[] status = fs.listStatus(new Path(args[0])); //input path containing file collection
+			for (int i = 0; i < status.length; i++) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
+				String lineRead;
 				lineRead = br.readLine();
+				String doc = "";
+
+				boolean value = false;
+				while (lineRead != null) {
+
+					if (value) {
+						doc += lineRead;
+
+					}
+					value = true;
+					lineRead = br.readLine();
+				}
+
+				String[] linesArray = doc.split("\\.");
+
+				Path pt2 = new Path(args[1]+"/file"+i); //output file path
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs.create(pt2, true)));
+				// TO append data to a file, use fs.append(Path f)
+
+				for (String line : linesArray) {
+					bw.write(line + ".");
+					bw.newLine();
+				}
+				bw.close();
 			}
-
-			String[] linesArray = doc.split("\\.");
-
-			Path pt2 = new Path(args[1]);
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs.create(pt2, true)));
-			// TO append data to a file, use fs.append(Path f)
-			
-			for (String line : linesArray) {
-				bw.write(line+".");
-				bw.newLine();
-			}
-			bw.close();
-
 
 		} catch (Exception e) {
 			e.printStackTrace();
